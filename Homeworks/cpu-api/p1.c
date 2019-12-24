b9b6b9e3f07e143b07d6906c7cad1cf311e1338c
Gilligan Hazard <gilligan.hazard@gmail.com>
2019-12-24 12:11:40 -0500

    Initial commit; README, .gitignore, unsophisticated Makefile, and cpu-api p1.

---
+++ Homeworks/cpu-api/p1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    int x = 100;
+    printf("x=%d\n", x);
+
+    int rc = fork();
+    if (rc < 0)
+    {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        printf("[child] x=%d\n", x);
+        printf("[child] assign new value to x\n");
+        x = 1;
+        printf("[child] x=%d\n", x);
+    }
+    else
+    {
+        printf("[parent] assign new value to x\n");
+        x = 10000;
+        printf("[parent] x=%d\n", x);
+    }
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+## OSTEP
+
+End-of-chapter homeworks from the one, the only [Operating Systems: Three Easy Pieces](http://pages.cs.wisc.edu/~remzi/OSTEP/).
