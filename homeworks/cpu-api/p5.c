3540fc7e81fcf9c755e8c8e17a70964e7b5469f5
Gilligan Hazard <gilligan.hazard@gmail.com>
2019-12-25 21:12:42 -0500

    cpu-api p5

---
+++ homeworks/cpu-api/p5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    int rc = fork();
+    if (rc < 0)
+    {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        // wait() returns an error if used in the child
+        printf("[child] pid: %d\n", (int)getpid());
+    }
+    else
+    {
+        // wait() returns the process ID of the terminated child
+        int wc = wait(NULL);
+        if (wc < 0)
+        {
+            fprintf(stderr, "wait failed\n");
+            exit(1);
+        }
+        printf("[parent] pid of terminated child: %d\n", wc);
+    }
+    return 0;
+}
