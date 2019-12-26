05871758e891dec9d13f8d11bf51cf4235ac6abf
Gilligan Hazard <gilligan.hazard@gmail.com>
2019-12-25 22:37:55 -0500

    cpu-api p7

---
+++ homeworks/cpu-api/p7.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[])
+{
+    int rc = fork();
+    if (rc < 0)
+    {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        close(STDOUT_FILENO);
+
+        // after closing the descriptor, output does not print
+        printf("%s", "gophers in space");
+    }
+    return 0;
+}
